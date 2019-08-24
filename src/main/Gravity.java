429c970ad46241ddf49ded9a723dd9ce4010289f
swatisaxena <swati.saxena_cs18@gla.ac.in>
2019-08-24 18:26:15 +0530

    initial commit variabkes declared

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -4,12 +4,18 @@
  * */
 package main;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in())
+        double time = sc.nextDouble();
+        double speed = 0.0;
+        double distance = 0.0;
+        double gravity = 9.8;
+
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
